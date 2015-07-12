5a9119f90506dc50cab8cd738f5aeee75917dd28
AD Slaton <slaton.ad@gmail.com>
2015-07-12 02:15:05 -0400

    Adding message posting

--- app.js
+++ app.js
@@ -19,6 +19,11 @@ mongoose.connect('mongodb://localhost:27017/messages', function mongooseConnecti
 /* Middleware */
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ 	next();
+});
 
 /* Routes */
 app.use('/messages', messages);
